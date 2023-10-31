6b08853198687768c1622deef6d38cbf2ea26075
Tom Draper <41476809+tom-draper@users.noreply.github.com>
2023-10-31 15:32:31 +0000

    Update analyze.go

--- pkg/analyze/analyze.go
+++ pkg/analyze/analyze.go
@@ -3,9 +3,43 @@ package analyze
 import (
 	"internal/server"
 
+	"reflect"
+
 	"github.com/tom-draper/log-analyzer/pkg/parse"
 )
 
+func dataTypeBreakdown(extraction []parse.Extraction) map[string]map[string]int {
+	dataTypes := make(map[string]map[string]int)
+	for _, e := range extraction {
+		for k, p := range e.Params {
+			dataType := reflect.TypeOf(p)
+			if _, ok := dataTypes[k]; !ok {
+				dataTypes[k] = make(map[string]int)
+			}
+			dataTypes[k][dataType.String()] += 1
+		}
+	}
+	return dataTypes
+}
+
+func failedLines(extraction []parse.Extraction) map[int]string {
+	failedLines := make(map[int]string)
+	for _, e := range extraction {
+		if len(e.Params) == 0 {
+			failedLines[e.LineNumber] = e.Line
+		}
+	}
+	return failedLines
+}
+
+func NewData(extraction []parse.Extraction, config *parse.Config) *server.Data {
+	dataTypes := dataTypeBreakdown(extraction)
+	failed := failedLines(extraction)
+	data := server.Data{Extraction: extraction, DataTypes: dataTypes, Failed: failed, Config: config}
+	return &data
+}
+
 func Run(extraction []parse.Extraction, config *parse.Config) {
-	server.Start(extraction, config)
+	data := NewData(extraction, config)
+	server.Start(data)
 }
